9634bbab9d3e56909a0e77c2c1a1739b0ee3ab04
lieutranhieuhoai-coder <lieutranhieuhoai@gmail.com>
2022-05-04 00:42:06 +0700

    login

---
+++ CarWash/CarWash/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CarWash.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CarWash.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
